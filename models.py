4503e0251b1f3b70f8058362d968d15fa95e33f7
IShamss <mohamedshams2000@gmail.com>
2020-05-22 22:22:39 +0200

    changed the database url

--- models.py
+++ models.py
@@ -5,7 +5,8 @@ import json
 # from flask_migrate import Migrate
 
 database_name = "moviesdb"
-database_path = "postgres://{}/{}".format('localhost:5433', database_name)
+#database_path = "postgres://{}/{}".format('localhost:5433', database_name)
+database_path = "postgres://hoxrmoutzdnnba:d8ecadcf1160dc0db268c43ca2df46351700676b4991dc576d3502795665aa2c@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d528hp4f3pirvb?sslmode=require"
 
 db = SQLAlchemy()
 
